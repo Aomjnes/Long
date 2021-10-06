4f5de6b207c9a5fc4fe4e1f1e4b46e8bd7930f8e
Aomjnes <85564525+Aomjnes@users.noreply.github.com>
2021-10-06 19:36:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    introTitle: 'Hoàng Anh à!',
+    introTitle: 'Bí à!',
     introDesc: `Rose is red.
     Violet is blue. 
     I just wanna ask.
     Do you love me too?`,
     btnIntro: 'Hello',
     title: 'This is for u',
-    desc: 'Just let me take care of you ',
+    desc: 'Hãy để em được phép thương chị nha',
     btnYes: 'Tất nhiên rồi',
     btnNo: 'Còn lâu',
-    question:'Rồi sau này tớ sẽ cưới cậu về nhé',
+    question:'Rồi sau này em sẽ cưới chị về nhé',
     btnReply: 'Gửi cho Long',
     reply: 'Được thôi,nhưng để sau này',
-    mess: 'Tớ rõ rồi.Cảm ơn Hoàng Anh',
+    mess: 'Em rõ rồi.Cảm ơn Bí nha',
     messDesc: 'Hẹn hôm nào đi cafe nhé',
     btnAccept: 'Okie :))',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
